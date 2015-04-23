fbddf7825c5a1bca7ec4b965f7941fac7b5806fb
evilkyro1965 <evilkyro1965@yahoo.com>
2015-04-24 06:47:43 +0700

    console readline

---
+++ ConsoleWriteReadLine/ConsoleWriteReadLine/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWriteReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 940;
+            int j = 73;
+
+            Console.WriteLine(" {0,4}\n+{1,4}\n ____\n {2,4}", i, j, i + j);
+            Console.ReadKey();
+        }
+    }
+}
